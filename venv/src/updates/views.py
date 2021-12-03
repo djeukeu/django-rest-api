dca77fc9836e18dc7f857ac81235163d979a466b
djeukeu <dmxchristian@gmail.com>
2021-12-03 23:09:19 +0100

    last course

--- venv/src/updates/views.py
+++ venv/src/updates/views.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
+
 from .models import Update
+from cfiapi.mixins import JsonResponseMixin
+import json
 
 
 def json_example_view(request):
@@ -8,4 +12,24 @@ def json_example_view(request):
         "count": 1000,
         "content": "Some new content"
     }
-    return JsonResponse(data)
+    json_data = json.dumps(data)
+    # return JsonResponse(data)
+    return HttpResponse(json_data, content_type='application/json')
+
+
+class JsonCBV(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return JsonResponse(data)
+
+
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "context": "Some new content"
+        }
+        return self.render_to_json_response(data)
